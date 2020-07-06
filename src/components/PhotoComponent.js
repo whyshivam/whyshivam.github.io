7f07f4bc2a70b4399b1e828505bbb91d508e59f6
Shivam Kumar <shivamkr.vatsa@gmail.com>
2020-07-06 17:59:05 +0530

    display mobile deployed

--- src/components/PhotoComponent.js
+++ src/components/PhotoComponent.js
@@ -1,45 +1,45 @@
 import React from 'react';
 //import {Row,Col} from 'reactstrap';
 function Photo(props) {
- return(
-   <div className="container page">
-   <div className="row row-header">
-   <div className="fib col-6"><h1 className="row-header" >Photo</h1></div>
-                <div className="fib col-6 page-icons" style={{textAlign:"right"}}>
-                    <a className="header-icons" rel="noopener noreferrer" target="_blank" href="https://instagram.com/whyshivam">
-                        <i className="fa fa-instagram"/>
-                    </a>
-                    <a className="header-icons" rel="noopener noreferrer" target="_blank" href="https://www.flickr.com/photos/188692070@N04/">
-                        <i className="fa fa-flickr"/>
-                    </a>
-                </div>
+   return (
+      <div className="container page">
+         <div className="row row-header">
+            <div className="fib col-6"><h1 className="row-header" >Photo</h1></div>
+            <div className="fib col-6 page-icons" style={{ textAlign: "right" }}>
+               <a className="header-icons" rel="noopener noreferrer" target="_blank" href="https://instagram.com/whyshivam">
+                  <i className="fa fa-instagram" />
+               </a>
+               <a className="header-icons" rel="noopener noreferrer" target="_blank" href="https://www.flickr.com/photos/188692070@N04/">
+                  <i className="fa fa-flickr" />
+               </a>
+            </div>
 
-      <div className="fib col-md-7  col-sm-12"  >
-         <div className="container-video fib1 pic">
-         <img alt="pic" src="/images/photo/fib1.png"/>
-         </div>
-      </div>
-      <div className="fib col-md-5 col-sm-12 row" >
-         <div className="fib col-md-12 col-sm-9">
-            <div className=" container-video fib1 pic">
-               <img alt="pic" src="/images/photo/fib2.png"/>
-            </div>
-         </div>
-         <div className="fib col-md-12 col-sm-3 row">
-            <div className="fib col-md-6 col-sm-12" >
+            <div className="fib col-md-7  col-sm-12"  >
                <div className="container-video fib1 pic">
-                  <img alt="pic" src="/images/photo/fib3.png"/>
+                  <img alt="pic" src="/images/photo/fib1.png" />
                </div>
             </div>
-            <div className="fib col-md-6 col-sm-12" >
-               <div className="container-video fib1 pic">
-                <img alt="pic" src="/images/photo/fib4.png"/>
+            <div className="fib col-md-5 col-sm-12 row" >
+               <div className="fib col-md-12 col-sm-9">
+                  <div className=" container-video fib1 pic">
+                     <img alt="pic" src="/images/photo/fib2.png" />
+                  </div>
+               </div>
+               <div className="fib col-md-12 col-sm-3 row">
+                  <div className="fib col-md-6 col-sm-12" >
+                     <div className="container-video fib1 pic">
+                        <img alt="pic" src="/images/photo/fib3.png" />
+                     </div>
+                  </div>
+                  <div className="fib col-md-6 col-sm-12" >
+                     <div className="container-video fib1 pic">
+                        <img alt="pic" src="/images/photo/fib4.png" />
+                     </div>
+                  </div>
                </div>
             </div>
          </div>
       </div>
-   </div>
-</div>
- );
+   );
 }
 export default Photo;
