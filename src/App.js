9f2931a8d6dcb3f350be7dca6e8e5dc3b3952caf
Shivam Kumar <shivamkr.vatsa@gmail.com>
2020-07-19 23:33:56 +0530

    particles design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
 import React,{Component} from 'react';
 import Main from './components/MainComponent'
 import './App.css';
+import Particles from 'react-particles-js';
 import {BrowserRouter} from 'react-router-dom'
 class App extends Component{
 
   render(){
     return(
       <BrowserRouter>
-        
+        <Particles
+    params={{
+	    "particles": {
+	        "number": {
+	            "value": 160,
+	            "density": {
+	                "enable": false
+	            }
+	        },
+	        "size": {
+	            "value": 3,
+	            "random": true,
+	            "anim": {
+	                "speed": 4,
+	                "size_min": 0.3
+	            }
+	        },
+	        "line_linked": {
+	            "enable": false
+	        },
+	        "move": {
+	            "random": true,
+	            "speed": 1,
+	            "direction": "top",
+	            "out_mode": "out"
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "bubble"
+	            },
+	            "onclick": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        },
+	        "modes": {
+	            "bubble": {
+	                "distance": 250,
+	                "duration": 2,
+	                "size": 0,
+	                "opacity": 0
+	            },
+	            "repulse": {
+	                "distance": 400,
+	                "duration": 4
+	            }
+	        }
+	    }
+	}} />
+
           <Main/>
           
         
