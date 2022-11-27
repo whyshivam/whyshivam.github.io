78883f7a032e95d996dce7cd1e9bcf8f8d8db18b
whyshivam <shivamkr.vatsa@gmail.com>
2022-11-27 19:02:05 +0530

    Adding chevron to page content

--- src/Components/PageContent.js
+++ src/Components/PageContent.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { scryRenderedComponentsWithType } from "react-dom/test-utils";
-
-
+import logo from "../logo.svg";
+import right from "../assets/next-arrow.svg"
 function PageContent(props) {
     const [pagename] = useState(props.pagename);
     const [pageloc] = useState(props.pageloc);
@@ -12,12 +11,19 @@ function PageContent(props) {
             <div className="page_content_container container">
                 <div className="page_content_header"> <h3>{pagename}</h3> </div>
                 <div className="page_content_flexbox">
+                    <div className="page_content_prev">
+                    <img className= "page_content_icon" src ={right} ></img>
+                    </div>
                     <div className="page_content_data">{pageloc}
                         loreumf dsf sdkfldsmkfdklsfjdngfjndslk ngkjdfnkgn dsnkjgnjd s
+                        gdfngkj ngkjdfnkgn fdngj defineloreumf dsf sdkfldsmkfdklsfjdngfjndslk ngkjdfnkgn dsnkjgnjd s
                         gdfngkj ngkjdfnkgn fdngj define
                     </div>
                     <div className="page_content_image">
-                        <img src="src/logo.svg"></img>
+                        <img src={logo}></img>
+                    </div>
+                    <div className="page_content_next"> 
+                    <img className= "page_content_icon" src ={right} ></img>
                     </div>
                 </div>
 
