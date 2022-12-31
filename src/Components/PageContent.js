4385f0058f0cae6503a9537b64289bb6ce0f73b0
whyshivam <shivamkr.vatsa@gmail.com>
2022-12-31 18:20:03 +0530

    View Type toggle

--- src/Components/PageContent.js
+++ src/Components/PageContent.js
@@ -8,7 +8,7 @@ function PageContent(props) {
     const [datas] = useState(require('./../db/' + pageloc));
     const [subLen] = useState(datas.length);
     const [subPagenum, setSubPagenum] = useState(0);
-    const [imgView, setImgView] = useState(subLen > 1)
+    const [viewType, setViewType] = useState(subLen > 1 ? "imageView" : "textView")
     const incrementCount = () => {
         setSubPagenum((subPagenum + 1) % datas.length);
     }
@@ -18,9 +18,9 @@ function PageContent(props) {
     const subImageClick = (event, idx) => {
         console.log(event);
         setSubPagenum(idx);
-        setImgView(false);
+        setViewType("textView");
     }
-    function getPics(activePage) {
+    const getPics = (activePage) => {
         var picloc = datas[activePage].pics;
         return (
             <div className="page_content_image" onClick={(event) => subImageClick(event, activePage)}>
@@ -31,23 +31,39 @@ function PageContent(props) {
     return (
         <div id={pageid} className="page_content ">
             <div className="page_content_container container">
-                <div className="page_content_header"> <h3>{pagename}</h3> </div>
+            <div className="page_content_header">
+                        <h3>{pagename}</h3>
+                        <div className="page_content_header_icons">
+                            {
+                                ["text", "multi", "image"].map((element) => {
+                                    var name = element + "View"
+                                    var isDisabled = (viewType === name ? " activeIcon" : " disabledIcon") + " "
+                                    var assetURL = "/assets/" + name + ".svg"
+                                    return (
+                                        <img className={name + "_icon" + isDisabled} src={assetURL} 
+                                            onClick={()=>setViewType(name)} alt={name} />
+                                    );
+                                })
+                            }
+
+                        </div>
+                    </div>
                 <div className="page_content_flexbox">
                     <div className="page_content_prev" onClick={decrementCount}>
                         <img className="page_content_icon" alt="" src="/assets/next-arrow.svg" ></img>
                     </div>
                     <div className="page_content_data_container">
-                        <div className="page_content_data" hidden={imgView}>
+                        <div className="page_content_data" hidden={viewType !== "textView"}>
 
                             {datas[subPagenum].content}
 
                         </div>
-                        {   imgView?[...Array(3).keys()].map((element, index) => {
-                                var activePage = (subPagenum + index )% subLen;
-                                return getPics(activePage)
-                            }): getPics(subPagenum)
+                        {(viewType === "imageView") ? [...Array(subLen>3?3:subLen).keys()].map((element, index) => {
+                            var activePage = (subPagenum + index) % subLen;
+                                  return getPics(activePage)
+                        }) : getPics(subPagenum)
                         }
-                        
+
                     </div>
 
                     <div className="page_content_next" onClick={incrementCount}>
